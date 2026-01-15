f85f838589149dde8ee180c029d6eab8ee7816cd
ElBojo44 <109545361+ElBojo44@users.noreply.github.com>
2026-01-14 21:09:16 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "v17"; // cambia este valor cada vez que hagas cambios
+const VERSION = "v18"; // cambia este valor cada vez que hagas cambios
 
 self.addEventListener("install", () => {
   console.log("Service Worker instalado:", VERSION);
